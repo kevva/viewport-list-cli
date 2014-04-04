e2a0b05e2d9ddee26915e40be704277fbed3c716
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-04 14:16:02 +0200

    Accept an array of keywords
    Fixes #1.

--- test.js
+++ test.js
@@ -5,9 +5,15 @@ var assert = require('assert');
 var viewport = require('./');
 
 describe('viewport()', function () {
-    it('should return two viewports', function (cb) {
+    it('should return viewports', function (cb) {
         viewport('iphone4', function (err, res) {
             cb(assert.strictEqual(res.length, 2));
+        });
+    });
+
+    it('should return viewports using an array of keywords', function (cb) {
+        viewport(['iphone4', 'iphone5'], function (err, res) {
+            cb(assert.strictEqual(res.length, 5));
         });
     });
 });
