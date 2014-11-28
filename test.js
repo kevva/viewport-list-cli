7908a0c474fe5bbf97cabd64d8bb0a84d08611dc
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-28 16:32:09 +0100

    Add CLI test

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var spawn = require('child_process').spawn;
 var test = require('ava');
 var viewport = require('./');
 
@@ -19,4 +20,18 @@ test('return all viewports', function (t) {
 		t.assert(!err);
 		t.assert(res.length > 50);
 	});
+});
+
+test('return viewports using the CLI', function (t) {
+	t.plan(2);
+
+	var cp = spawn(__dirname + '/cli.js');
+
+	cp.stdout.setEncoding('utf8');
+	cp.stdout.on('data', function (data) {
+		t.assert(data);
+		t.assert(data.indexOf('iphone 4,ios,5.1.1,320x480,2010-06') !== -1);
+	});
+
+	cp.stdin.end('iphone4');
 });
