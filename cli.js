fe4bccec7ba1ee1057f603a1d9f1ff46f0fd06ee
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-07-08 11:56:44 +0530

    Show the output as a well-formatted table.
    - The width of the column is calculated dynamically using the width of
      the terminal that the user is using.
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@
 var meow = require('meow');
 var getStdin = require('get-stdin');
 var toCsv = require('to-csv');
+var Table = require('cli-table');
+var windowSize = require('window-size');
 var viewport = require('viewport-list');
 
 var cli = meow({
@@ -17,19 +19,34 @@ var cli = meow({
 	]
 });
 
-function run(input) {
+function run(input, flags) {
 	viewport(input, function (err, devices) {
 		if (err) {
 			console.error(err.message);
 			process.exit(1);
 		}
 
-		console.log(toCsv(devices));
+		if (flags && (flags.pretty || flags.p)){
+			var colWidth = Math.floor(windowSize.width / 6);
+			var table = new Table({
+			    head: ['name', 'platform', 'os', 'size', 'release']
+			  , colWidths: [colWidth, colWidth, colWidth, colWidth, colWidth]
+			});
+
+			devices.forEach(function (device, index, array) {
+				table.push(
+					[device.name, device.platform, device.os, device.size, device.release]
+				)
+			});
+			console.log(table.toString());
+		} else {
+			console.log(toCsv(devices));
+		}
 	});
 }
 
 if (process.stdin.isTTY) {
-	run(cli.input);
+	run(cli.input, cli.flags);
 } else {
 	getStdin(function (data) {
 		[].push.apply(cli.input, data.trim().split('\n'));
